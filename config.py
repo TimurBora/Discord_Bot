a8da2eddabaf42a854b312a574d3ab5ffd65ffae
unknown <timurborisov5561@gmail.com>
2023-03-24 21:56:39 +0100

    Не понятно что добавил, но наговнокодил знатно

--- config.py
+++
@@ -1,8 +0,0 @@
-settings = {
-    'token': 'bae504ccca75616d0ab561807ea95e94b2b0e41d13f84edeef2e3f72c38b71ca',
-    'bot': 'Test_Bot',
-    'id': 1083721004926382090,
-    'prefix': '!'
-}
-#MTA4MzcyMTAwNDkyNjM4MjA5MA.GKzblP.V7gHuUx4V6o32lNymFxkrfD_5rOjN1BYF4IqiE
-#OPENAI API KEY: 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
